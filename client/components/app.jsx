319dc6593fa01562b9725ed8c3619a07ff9524f7
Lily Li <lily.li0597@gmail.com>
2019-03-08 20:15:04 -0500

    feat(cat) added cat that moves with wasd

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,60 +3,11 @@ import React from 'react';
 export default class App extends React.Component {
   constructor() {
     super()
-    this.state = {
-      text: '',
-      messages: []
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.scrollToBottom = this.scrollToBottom.bind(this)
-  }
-  
-  componentDidUpdate() {
-    this.scrollToBottom()
   }
 
-  handleChange(evt) {
-    this.setState({text: evt.target.value})
-  }
-
-  handleSubmit(evt) {
-    evt.preventDefault()
-
-    this.setState({
-      text: '',
-      messages: [...this.state.messages, this.state.text]
-    })
-  }
-
-  scrollToBottom() {
-    this.el.scrollIntoView(false)
-  }
-  
   render() {
     return (
-      <div id = 'chatbox-container'>
-        <div id ='message-container'>
-          <div className = 'message-catbot'>
-            hello, world!
-          </div>
-          <div className='message-duckbot'>
-            hello, world!
-          </div>
-          {this.state.messages.map((message, idx) => (
-            <div className = 'message-user' key = {idx}>
-              {message}
-            </div>
-          ))}
-          <div ref = {el => { this.el = el }} />
-        </div>
-        
-        <form id = 'input-container' onSubmit = {this.handleSubmit}>
-          <input type = 'text' value = {this.state.text} onChange = {this.handleChange} />
-          <button type = 'submit'>send</button>
-        </form>
-      </div>
-    );
+      <div id='cat' />
+    )
   }
 }
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import { app, BrowserWindow } from 'electron';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
+import localShortcut from 'electron-localshortcut';
+// var Mousetrap = require('mousetrap');
+// import keyboardJS from 'keyboardjs';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let attackWin;
 
 const isDevMode = process.execPath.match(/[\\/]electron/);
 
 if (isDevMode) enableLiveReload({ strategy: 'react-hmr' });
 
-const createWindow = async () => {
+let createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 300,
-    height: 300,
+    x: 0,
+    y: 0,
+    width: 210,
+    height: 205,
+    alwaysOnTop: true,
     transparent: true,
     frame: false
   });
@@ -23,10 +30,10 @@ const createWindow = async () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(REACT_DEVELOPER_TOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+  // if (isDevMode) {
+  //   await installExtension(REACT_DEVELOPER_TOOLS);
+  //   mainWindow.webContents.openDevTools();
+  // }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
@@ -61,3 +68,53 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+app.on('ready', () => {
+  // console.log(Mousetrap)
+  // Mousetrap.bind('w', () => {
+  //   const coord = mainWindow.getPosition()
+  //   const x = coord[0]
+  //   const y = coord[1] - 10
+
+  //   mainWindow.setPosition(x, y)
+  // })
+  // keyboardJS.bind('w', e => {
+  //   console.log('w')
+  // })
+
+  // window.addEventListener('w', e => {
+  //   console.log('w')
+  // }, true)
+
+  localShortcut.register(mainWindow, 'W', () => {
+    const coord = mainWindow.getPosition()
+    const x = coord[0]
+    const y = coord[1] - 10
+
+    mainWindow.setPosition(x, y)
+  })
+
+  localShortcut.register(mainWindow, 'A', () => {
+    const coord = mainWindow.getPosition()
+    const x = coord[0] - 10
+    const y = coord[1]
+
+    mainWindow.setPosition(x, y)
+  })
+
+  localShortcut.register(mainWindow, 'S', () => {
+    const coord = mainWindow.getPosition()
+    const x = coord[0]
+    const y = coord[1] + 10
+
+    mainWindow.setPosition(x, y)
+  })
+
+  localShortcut.register(mainWindow, 'D', () => {
+    const coord = mainWindow.getPosition()
+    const x = coord[0] + 10
+    const y = coord[1]
+
+    mainWindow.setPosition(x, y)
+  })
+})
