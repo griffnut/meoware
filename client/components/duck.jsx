4c745bbfdcaed16123dba48d6f891e7bd4c8fa89
Lily Li <lily.li0597@gmail.com>
2019-04-12 17:07:18 -0400

    fix(duck) changing direction less glitchy

--- client/components/duck.jsx
+++ client/components/duck.jsx
@@ -11,10 +11,11 @@ export default class Duck extends React.Component {
     }
 
     this.contextMenu = this.contextMenu.bind(this)
+    this.move = this.move.bind(this)
+    this.stop = this.stop.bind(this)
   }
 
   componentDidMount() {
-    const duckWindow = remote.getCurrentWindow()
     Mousetrap.bind('q', () => {
       const audio = document.createElement('audio');
       audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav';
@@ -22,112 +23,32 @@ export default class Duck extends React.Component {
       audio.play();
     })
 
-    Mousetrap.bind('w', () => {
-      this.setState({ducky: 'bducky-walk'})
-      const coord = duckWindow.getPosition()
-      const x = coord[0]
-      const y = coord[1] - 10
+    Mousetrap.bind(['up', 'w'], () => this.move(0, -10, 'bducky'), 'keydown')
+    Mousetrap.bind(['up', 'w'], () => this.stop('bducky'), 'keyup')
 
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('w', () => this.setState({ducky: 'bducky'}), 'keyup')
+    Mousetrap.bind(['left', 'a'], () => this.move(-10, 0, 'lducky'), 'keydown')
+    Mousetrap.bind(['left', 'a'], () => this.stop('lducky'), 'keyup')
 
-    Mousetrap.bind('a', () => {
-      this.setState({ ducky: 'lducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0] - 10
-      const y = coord[1]
+    Mousetrap.bind(['down', 's'], () => this.move(0, 10, 'fducky'), 'keydown')
+    Mousetrap.bind(['down', 's'], () => this.stop('fducky'), 'keyup')
 
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('a', () => this.setState({ ducky: 'lducky' }), 'keyup')
-
-    Mousetrap.bind('s', () => {
-      this.setState({ ducky: 'fducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0]
-      const y = coord[1] + 10
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('s', () => this.setState({ ducky: 'fducky' }), 'keyup')
-
-    Mousetrap.bind('d', () => {
-      this.setState({ ducky: 'rducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0] + 10
-      const y = coord[1]
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('d', () => this.setState({ ducky: 'rducky' }), 'keyup')
-
-    Mousetrap.bind('q', () => {
-      const audio = document.createElement('audio');
-      audio.src = 'http://www.wavlist.com/soundfx/011/duck-baby.wav';
-      audio.load();
-      audio.play();
-    })
-
-    Mousetrap.bind('up', () => {
-      this.setState({ ducky: 'bducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0]
-      const y = coord[1] - 10
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('up', () => this.setState({ ducky: 'bducky' }), 'keyup')
-
-    Mousetrap.bind('left', () => {
-      this.setState({ ducky: 'lducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0] - 10
-      const y = coord[1]
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('left', () => this.setState({ ducky: 'lducky' }), 'keyup')
-
-    Mousetrap.bind('down', () => {
-      this.setState({ ducky: 'fducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0]
-      const y = coord[1] + 10
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('down', () => this.setState({ ducky: 'fducky' }), 'keyup')
-
-    Mousetrap.bind('right', () => {
-      this.setState({ ducky: 'rducky-walk' })
-      const coord = duckWindow.getPosition()
-      const x = coord[0] + 10
-      const y = coord[1]
-
-      duckWindow.setPosition(x, y)
-    })
-    Mousetrap.bind('right', () => this.setState({ ducky: 'rducky' }), 'keyup')
+    Mousetrap.bind(['right', 'd'], () => this.move(10, 0, 'rducky'), 'keydown')
+    Mousetrap.bind(['right', 'd'], () => this.stop('rducky'), 'keyup')
   }
 
-  componentWillUnmount() {
-    Mousetrap.unbind('q')
-    Mousetrap.unbind('up')
-    Mousetrap.unbind('up', 'keyup')
-    Mousetrap.unbind('left')
-    Mousetrap.unbind('left', 'keyup')
-    Mousetrap.unbind('down')
-    Mousetrap.unbind('down', 'keyup')
-    Mousetrap.unbind('right')
-    Mousetrap.unbind('right', 'keyup')
-    Mousetrap.unbind('w')
-    Mousetrap.unbind('w', 'keyup')
-    Mousetrap.unbind('a')
-    Mousetrap.unbind('a', 'keyup')
-    Mousetrap.unbind('s')
-    Mousetrap.unbind('s', 'keyup')
-    Mousetrap.unbind('d')
-    Mousetrap.unbind('d', 'keyup')
+  move(moveX, moveY, dir) {
+    const duckWindow = remote.getCurrentWindow()
+
+    this.setState({ ducky: `${dir}-walk` })
+    const coord = duckWindow.getPosition()
+    const x = coord[0] + moveX
+    const y = coord[1] + moveY
+
+    duckWindow.setPosition(x, y)
+  }
+
+  stop(name) {
+    if (this.state.ducky === `${name}-walk`) this.setState({ ducky: name })
   }
 
   contextMenu(e) {
